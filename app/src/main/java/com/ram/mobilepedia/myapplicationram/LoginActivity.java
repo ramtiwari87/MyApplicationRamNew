b92251f4605e09d46342949f303a02d63b971bbc
Ram <ram.tiwari@mobilepedia.in>
2017-11-09 16:47:24 +0530

    Correct

--- app/src/main/java/com/ram/mobilepedia/myapplicationram/LoginActivity.java
+++ app/src/main/java/com/ram/mobilepedia/myapplicationram/LoginActivity.java
@@ -49,7 +49,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      *
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "foo@example.com:hello", "bar@example.com:world","ram.tiwari@mobilepedia.in"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
